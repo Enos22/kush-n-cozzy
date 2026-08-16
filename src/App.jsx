2c6d95732e316a87650b1fd51d51e8c066bfa0c9
Emos Arenga <enos.arenga@gmail.com>
2026-08-16 22:53:53 +0300

    rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Home from './components/Home'
+import { createBrowserRouter, Routes, Route, BrowserRouter } from 'react-router-dom'
+import ProductList from './pages/ProductList'
 
 import './App.css'
 
@@ -6,6 +9,15 @@ import './App.css'
 function App() {
 
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Products' element={<ProductList />} />
+
+
+      </Routes>
+
+    </BrowserRouter>
 
   )
 }
